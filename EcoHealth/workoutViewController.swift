34c37d9da571d2e86bd19615568bb09259f92add
Shivika Varshney <shivika.colors@gmail.com>
2022-08-03 21:19:57 -0400

    workouts

--- EcoHealth/workoutViewController.swift
+++ EcoHealth/workoutViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class workoutViewController: UIViewController {
 
-    var workouts = ["do strength training with 10 pound dumbells"]
+    var workouts = ["Go for a 30 minute run outside at a nice, slow pace! Running outside lets you save electricity by not using a treadmill. Remember to stretch before and after. Check out what to eat after your workout to make sure you're getting the benefits of your run.", "Run a 5K - and try to get under 35 minutes!", "Use dumbells at home and try doing 3 sets (20 reps each) of bicep curls, tricep curls, tricep dips, hammer curls, "]
     override func viewDidLoad() {
         super.viewDidLoad()
     }
