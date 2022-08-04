f411298d31be80e6f24b87ecce74f4832c7eec69
Shivika Varshney <shivika.colors@gmail.com>
2022-08-03 21:41:15 -0400

    workout page is working!

--- EcoHealth/workoutViewController.swift
+++ EcoHealth/workoutViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class workoutViewController: UIViewController {
-
-    var workouts = ["Go for a 30 minute run outside at a nice, slow pace! Running outside lets you save electricity by not using a treadmill. Remember to stretch before and after. Check out what to eat after your workout to make sure you're getting the benefits of your run.", "Run a 5K - and try to get under 35 minutes!", "Use dumbells at home and try doing 3 sets (20 reps each) of bicep curls, tricep curls, tricep dips, hammer curls, "]
+    @IBOutlet weak var workout: UILabel!
+    
+    var workouts = ["Go for a 30 minute run outside at a nice, slow pace! Running outside lets you save electricity by not using a treadmill.", "Run a 5K - and try to get under 35 minutes!", "Use dumbells at home (you can shop secondhand or buy adjustable dumbells) and try doing 3 sets (20 reps each) of bicep curls, tricep curls, tricep dips, hammer curls, overhead presses, deadlifts, and pullups. Then complete a core workout consisting of crunches, mountain climbers, bicycles, planks, and hip dips (you can also add your own exercises).", ""]
     override func viewDidLoad() {
         super.viewDidLoad()
+        workout.isHidden = true
     }
     
 
     @IBAction func create(_ sender: UIButton) {
+        workout.isHidden = false
+        let output = workouts.randomElement()
+        self.workout.text = output
     }
     
 }
